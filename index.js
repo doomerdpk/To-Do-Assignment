81cb76ababf05c7c096a8277ba89528bd11104ca
doomerdpk <doomerdpk@gmail.com>
2024-08-18 18:49:42 +0530

    Updated Sizes from px to rem

--- index.js
+++ index.js
@@ -39,19 +39,19 @@ function addTodo()
     }
 
     const itemHTML = `
-    <div style="margin-left: 400px; margin-bottom: 10px;" id="${ctr}">
+    <div style="margin-left: 25rem; margin-bottom: 0.625rem;" id="${ctr}">
         <div>
         <h4>${input_value}</h4>
         </div>
         <div>
 
-        <button style="margin-left: 10px; height:30px; background-color:white; border:none; border-radius: 4px;"
+        <button style="margin-left: 0.625rem; height:1.875rem; background-color:white; border:none; border-radius: 4px;"
          onclick="deleteToDo(${ctr})"><img width=25px; src='https://imgs.search.brave.com/s-VgHDbnEwiQ0sxfysp-9twQYvVOKpAckdyrREhJciE/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMuYWxsLWZyZWUt/ZG93bmxvYWQuY29t/L2ltYWdlcy90aHVt/YmpwZy90cmFzaF9h/bHRfc2lnbl9pY29u/X2ZsYXRfY29udHJh/c3RfYmxhY2tfd2hp/dGVfc3ltbWV0cmlj/X291dGxpbmVfNjky/MTg3My5qcGc'></button>
          
-         <button style="margin-left: 10px; height:32px; background-color:white; border:none; border-radius: 4px;"
+         <button style="margin-left: 0.625rem; height:2rem; background-color:white; border:none; border-radius: 4px;"
          onclick="EditToDo(${ctr})"><img width=25px; src='https://imgs.search.brave.com/gs217nxPES1qeGmClgerV8XPvDJIK9GIarSWJfOy78Y/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzAxLzk3LzIyLzk3/LzM2MF9GXzE5NzIy/OTc4Nl92V0VGcGVR/RU90SWNqdnRLVlJB/eVBQUDkxQU5zNDN1/cS5qcGc'></button>
          
-         <button style="margin-left: 10px; height:32px; background-color:white; border:none; border-radius: 4px;"
+         <button style="margin-left: 0.625rem; height:2rem; background-color:white; border:none; border-radius: 4px;"
          onclick="SaveToDo(${ctr})"><img width=25px; src='https://imgs.search.brave.com/7XQ1dtw0n_LJ_VWObMjTSDIL-obz02Sd0dB_znGDVyA/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly90My5m/dGNkbi5uZXQvanBn/LzA2Lzk0LzU3Lzc0/LzM2MF9GXzY5NDU3/NzQ2MF9MMXlQdjZz/bmZxZk05dzhWT1J5/TnBYRnRhNlEwcFRk/dS5qcGc'></button>
 
         </div>
